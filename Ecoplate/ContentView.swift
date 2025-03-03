78c2114a5c20f97fe7093fc1afc6512c3913aa68
Doğukan <dcatmakas@gmail.com>
2025-03-03 21:09:25 +0300

    first commit

---
+++ Ecoplate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ecoplate
+//
+//  Created by Doğukan Çatmakaş on 3.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Ecoplate!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ecoplate/EcoplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoplateApp.swift
+//  Ecoplate
+//
+//  Created by Doğukan Çatmakaş on 3.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct EcoplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
